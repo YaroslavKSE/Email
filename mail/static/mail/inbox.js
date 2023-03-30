40924027e84944bc62a10e45e7184b8cf4370e20
YaroslavKSE <ikhomych@kse.org.ua>
2023-03-30 20:30:34 +0300

    Added load the appropriate mailbox views, now user can see "Inbox" and "Sent" mails. Still need to add archive button function and "Archive" box view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -39,6 +39,7 @@ function load_mailbox(mailbox) {
         .then(response => response.json())
         .then(email => {
             // Print email
+            email.forEach(mail => show_email(mail))
             console.log(email);
 
             // ... do something else with email ...
@@ -68,6 +69,51 @@ function send_mail() {
     return false; // to avoid reloading inbox page
 }
 
-function show_email(){
+function show_email(mail) {
+    const emailDiv = document.createElement('div')
+    emailDiv.classList.add("card", "mb-3");
+    emailDiv.id = "email-card"
+
+    // Create a card header with the email sender and date
+    const headerDiv = document.createElement("div");
+    headerDiv.classList.add("card-header");
+    const senderElement = document.createElement("h5");
+    senderElement.classList.add("card-title", "mb-0");
+    senderElement.textContent = mail.sender;
+    const dateElement = document.createElement("small");
+    dateElement.classList.add("text-muted");
+    dateElement.textContent = mail.timestamp;
+    headerDiv.appendChild(senderElement);
+    headerDiv.appendChild(dateElement);
+    emailDiv.appendChild(headerDiv);
+
+    // Create a card body with the email topic
+    const bodyDiv = document.createElement("div");
+    bodyDiv.classList.add("card-body");
+    const topicElement = document.createElement("h6");
+    topicElement.classList.add("card-subtitle", "mb-2", "text-muted");
+    topicElement.textContent = mail.subject;
+    bodyDiv.appendChild(topicElement);
+    emailDiv.appendChild(bodyDiv);
+
+    // Create an archive button
+    const archiveButton = document.createElement("button");
+    archiveButton.classList.add("btn", "btn-secondary");
+    archiveButton.textContent = "Archive";
+    emailDiv.appendChild(archiveButton);
+
+    //Check if email is read
+    if (mail.read){
+        emailDiv.className = "card text-dark bg-light mb-3"
+    } else {
+        emailDiv.className = "card border-primary mb-3"
+    }
+    //card text-dark bg-light mb-3
+    //card border-dark mb-3
+
+
+    // Add the email div to the document
+    const containerDiv = document.querySelector('#emails-view');
+    containerDiv.appendChild(emailDiv);
     return false
 }
